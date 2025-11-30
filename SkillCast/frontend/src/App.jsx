a381defd36a3f96eebc5ab3d6c1d71dcb58689ea
Praveen1116 <kpraveen1116@gmail.com>
2025-11-30 13:18:39 +0530

    Fixed SPA routing

--- SkillCast/frontend/src/App.jsx
+++ SkillCast/frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { Index } from "./pages/Index"
-import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Index } from "./pages/Index";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Courses } from "./pages/Courses";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
@@ -12,23 +12,24 @@ import { UpdateCourse } from "./Admin/UpdateCourse";
 import { MyCourses } from "./pages/MyCourses";
 
 function App() {
-
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Index />}/>
-        <Route path="/courses" element={<Courses />}/>
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/my-courses" element={<MyCourses />} />
-        <Route path="/adminsignin" element={<AdminSignin />} />
-        <Route path="/adminsignup" element={<AdminSignup />} />
-        <Route path="/add-course" element={<AddCourse />} />
-        <Route path="/update-course" element={<UpdateCourse />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/my-courses" element={<MyCourses />} />
+          <Route path="/adminsignin" element={<AdminSignin />} />
+          <Route path="/adminsignup" element={<AdminSignup />} />
+          <Route path="/add-course" element={<AddCourse />} />
+          <Route path="/update-course" element={<UpdateCourse />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
